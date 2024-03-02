be46d2e37d5470026591846892bae766e7097822
richardy <richuchiha666@gmail.com>
2024-03-02 08:37:00 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,27 @@
+const form = document.getElementById('form-confirm');
+const inputA = document.getElementById('valor-a');
+const inputB = document.getElementById('valor-b');
+let formEvalido = false;
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const valorA = parseFloat(inputA.value);
+    const valorB = parseFloat(inputB.value);
+
+    if (valorB > valorA) {
+        
+        const containerSucesso = document.querySelector('.success-message');
+        containerSucesso.innerHTML = 'Formulário enviado com sucesso! Valor B é maior que Valor A!';
+        containerSucesso.style.display = 'block';
+        containerSucesso.classList.remove('error');
+        document.querySelector('.error-message').style.display = 'none';
+
+        
+        inputA.value = '';
+        inputB.value = '';
+    } else {
+        
+        document.querySelector('.error-message').style.display = 'block';
+    }
+});
